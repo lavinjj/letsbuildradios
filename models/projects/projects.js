b9feb2a4d3f989696e224b01761aa66726043a47
Jim Lavin <jlavin@jimlavin.net>
2015-05-21 23:57:45 -0500

    Added images for first 3 projects

--- models/projects/projects.js
+++ models/projects/projects.js
@@ -12,9 +12,9 @@ exports.Project = Project;
 var ProjectManager = (function () {
     function ProjectManager() {
         this.projects = [];
-        this.projects.push(new Project("The Let's Build Something Radio", "posts/LBS/intro.md", "Step-by-step guide to building the Let's Build Something SSB Transceiver", "img/icons/avatar-analog.png"));
-        this.projects.push(new Project("The Minima SSB Transceiver", "posts/Minima/intro.md", "Step-by-step guide to building the Minima SSB Transceiver", "img/icons/avatar-analog.png"));
-        this.projects.push(new Project("The SW40+ CW Transceiver", "posts/LBS/intro.md", "Step-by-step guide to building the Small Wonders CW Transceiver", "img/icons/avatar-analog.png"));
+        this.projects.push(new Project("The Let's Build Something Radio", "posts/LBS/intro.md", "Step-by-step guide to building the Let's Build Something SSB Transceiver", "img/icons/lbs.png"));
+        this.projects.push(new Project("The Minima SSB Transceiver", "posts/Minima/intro.md", "Step-by-step guide to building the Minima SSB Transceiver", "img/icons/minima.png"));
+        this.projects.push(new Project("The Sproutie Regen Receiver", "posts/Sproutie/intro.md", "Step-by-step guide to building the Sproutie Regen Receiver", "img/icons/sproutie.png"));
     }
     ProjectManager.prototype.addProject = function (project) {
         this.projects.push(project);
--- models/projects/projects.ts
+++ models/projects/projects.ts
@@ -18,9 +18,9 @@ export class Project{
 export class ProjectManager{
   public projects:Array<Project> = [];
   constructor(){
-    this.projects.push(new Project("The Let's Build Something Radio", "posts/LBS/intro.md", "Step-by-step guide to building the Let's Build Something SSB Transceiver", "img/icons/avatar-analog.png"));
-    this.projects.push(new Project("The Minima SSB Transceiver", "posts/Minima/intro.md", "Step-by-step guide to building the Minima SSB Transceiver", "img/icons/avatar-analog.png"));
-    this.projects.push(new Project("The SW40+ CW Transceiver", "posts/LBS/intro.md", "Step-by-step guide to building the Small Wonders CW Transceiver", "img/icons/avatar-analog.png"));
+    this.projects.push(new Project("The Let's Build Something Radio", "posts/LBS/intro.md", "Step-by-step guide to building the Let's Build Something SSB Transceiver", "img/icons/lbs.png"));
+    this.projects.push(new Project("The Minima SSB Transceiver", "posts/Minima/intro.md", "Step-by-step guide to building the Minima SSB Transceiver", "img/icons/minima.png"));
+    this.projects.push(new Project("The Sproutie Regen Receiver", "posts/Sproutie/intro.md", "Step-by-step guide to building the Sproutie Regen Receiver", "img/icons/sproutie.png"));
   }
 
   addProject(project:Project){
